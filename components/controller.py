ec9205645b8004d5b6fae8d6ba0ae110c67b868d
Greasytugboat6 <78366801+Greasytugboat6@users.noreply.github.com>
2024-02-15 18:54:34 -0600

    Create abstraction for controllers

---
+++ components/controller.py
@@ -0,0 +1,42 @@
+import wpilib
+from robot_map import USB
+
+
+class Controller:
+    def __init__(self):
+        self.JoystickCtrl = False
+        if self.JoystickCtrl:
+            self.hardware = wpilib.Joystick(USB.controller1Channel)
+        else:
+            self.hardware= wpilib.XboxController(USB.controller1Channel)
+    
+    def getLeftY(self):
+        if self.JoystickCtrl:
+            return self.hardware.getY()
+        else:
+            return self.hardware.getLeftY()
+    
+    def getLeftX(self):
+        if self.JoystickCtrl:
+            return self.hardware.getX()
+        else:
+            return self.hardware.getLeftX()
+    
+    def getRightY(self):
+        if self.JoystickCtrl:
+            return self.hardware.getX()
+        else:
+            return self.hardware.getRightX()
+    
+    def getBackButton(self):
+        if self.JoystickCtrl:
+            return None
+        else:
+            return self.hardware.getBackButton()
+    
+    def getLeftBumper(self):
+        if self.JoystickCtrl:
+            return None
+        else:
+            return self.hardware.getLeftBumper
+        
