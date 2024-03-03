73014e024f4aeb29defdc30a5912b88377ceb176
Markus Hoehn <markushoehn2007@gmail.com>
2024-03-03 14:16:46 -0600

    rename

--- autonomous/one_note_stage.py
+++ autonomous/one_note_stage.py
@@ -1,8 +1,8 @@
 from robotpy_ext.autonomous import StatefulAutonomous, state, timed_state
 
 class Autonomous(StatefulAutonomous):
-    MODE_NAME = "TWO_NOTE_LEFT"
-    # tx: 7.12 ty: 8.8
+    MODE_NAME = "ONE_NOTE_STAGE"
+    # tx: -7.12 ty: 8.8
 
     def intialize(self):
         self.intial_called = None
@@ -24,7 +24,7 @@ class Autonomous(StatefulAutonomous):
         
     @timed_state(duration=0.4, next_state="next")
     def rotate(self):
-        self.DriveTrain.robotDrive.driveCartesian(0, 0, 0.3)
+        self.DriveTrain.robotDrive.driveCartesian(0, 0, -0.3)
 
     @timed_state(duration=1, next_state="backup")
     def next(self):
@@ -33,49 +33,29 @@ class Autonomous(StatefulAutonomous):
         self.Intake.intakeMotorTwo.set(0)
 
 
-    @timed_state(duration=2, next_state="pause")
+    @timed_state(duration=0.6, next_state="strafe")
     def backup(self):
         self.Shooter.shooterMotor.set(0)
         self.Intake.intakeMotorOne.set(self.Intake.intakeInSpeed)
         self.Intake.intakeMotorTwo.set(self.Intake.intakeInSpeed)
         self.DriveTrain.robotDrive.driveCartesian(0.3, 0, 0)
 
-    @timed_state(duration=1, next_state="forward")
-    def pause(self):
+    @timed_state(duration=1.5, next_state="intake_up")
+    def strafe(self):
+        self.DriveTrain.robotDrive.driveCartesian(0, 0.3, 0)
+
+    @timed_state(duration=1, next_state="drive")
+    def intake_up(self):
         self.Intake.pivotOnePosition = 0.1
         self.Intake.pivotTwoPosition = 0.1
-        self.DriveTrain.robotDrive.driveCartesian(0, 0, 0)
 
-    @timed_state(duration=2, next_state="rotate2")
-    def forward(self):
-        self.DriveTrain.robotDrive.driveCartesian(-0.3, 0, 0)
-
-    @timed_state(duration=0.6, next_state="pause2")
-    def rotate2(self):
-        self.DriveTrain.robotDrive.driveCartesian(0, 0, -0.3)
-
-    @timed_state(duration=1, next_state="small_forward")
-    def pause2(self):
+    @timed_state(duration=1.3, next_state="stop")
+    def drive(self):
         self.Intake.intakeMotorOne.set(0)
         self.Intake.intakeMotorTwo.set(0)
-        self.DriveTrain.robotDrive.driveCartesian(0, 0, 0)
+        self.DriveTrain.robotDrive.driveCartesian(0.3, 0, 0)
 
-    @timed_state(duration=0.7, next_state="start2")
-    def small_forward(self):
-        self.DriveTrain.robotDrive.driveCartesian(-0.3, 0, 0)
-
-    @timed_state(duration=2, next_state="fire2")
-    def start2(self):
-        self.Shooter.shooterMotor.set(self.Shooter.shooterSpeed)
-
-    @timed_state(duration=1, next_state="stop")
-    def fire2(self):
-        self.Intake.intakeMotorOne.set(-self.Intake.intakeOutSpeed)
-        self.Intake.intakeMotorTwo.set(-self.Intake.intakeOutSpeed)
-
-    @timed_state(duration=2)
+    @timed_state(duration=1)
     def stop(self):
-        self.Intake.intakeMotorOne.set(0)
-        self.Intake.intakeMotorTwo.set(0)
-        self.Shooter.shooterMotor.set(0)
         self.DriveTrain.robotDrive.driveCartesian(0, 0, 0)
+
--- autonomous/two_note_stage.py
+++ autonomous/two_note_stage.py
@@ -1,8 +1,8 @@
 from robotpy_ext.autonomous import StatefulAutonomous, state, timed_state
 
 class Autonomous(StatefulAutonomous):
-    MODE_NAME = "ONE_NOTE_LEFT"
-    # tx: 7.12 ty: 8.8
+    MODE_NAME = "TWO_NOTE_STAGE"
+    # tx: -7.12 ty: 8.8
 
     def intialize(self):
         self.intial_called = None
@@ -24,7 +24,7 @@ class Autonomous(StatefulAutonomous):
         
     @timed_state(duration=0.4, next_state="next")
     def rotate(self):
-        self.DriveTrain.robotDrive.driveCartesian(0, 0, 0.3)
+        self.DriveTrain.robotDrive.driveCartesian(0, 0, -0.3)
 
     @timed_state(duration=1, next_state="backup")
     def next(self):
@@ -33,17 +33,29 @@ class Autonomous(StatefulAutonomous):
         self.Intake.intakeMotorTwo.set(0)
 
 
-    @timed_state(duration=2, next_state="stop")
+    @timed_state(duration=0.6, next_state="strafe")
     def backup(self):
         self.Shooter.shooterMotor.set(0)
         self.Intake.intakeMotorOne.set(self.Intake.intakeInSpeed)
         self.Intake.intakeMotorTwo.set(self.Intake.intakeInSpeed)
         self.DriveTrain.robotDrive.driveCartesian(0.3, 0, 0)
 
-    @timed_state(duration=2)
-    def stop(self):
+    @timed_state(duration=1.5, next_state="intake_up")
+    def strafe(self):
+        self.DriveTrain.robotDrive.driveCartesian(0, 0.3, 0)
+
+    @timed_state(duration=1, next_state="drive")
+    def intake_up(self):
         self.Intake.pivotOnePosition = 0.1
         self.Intake.pivotTwoPosition = 0.1
+
+    @timed_state(duration=1.3, next_state="stop")
+    def drive(self):
         self.Intake.intakeMotorOne.set(0)
         self.Intake.intakeMotorTwo.set(0)
+        self.DriveTrain.robotDrive.driveCartesian(0.3, 0, 0)
+
+    @timed_state(duration=1)
+    def stop(self):
         self.DriveTrain.robotDrive.driveCartesian(0, 0, 0)
+
